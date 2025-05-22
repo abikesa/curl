f393e4b7a586f88a54465fcc85f9c840a40f40eb
abikesa <144293697+abikesa@users.noreply.github.com>
2025-05-22 19:21:24 -0400

    nuke.sh

--- syntax.sh
+++ syntax.sh
@@ -1,1 +1,2 @@
-curl -fsSL https://raw.githubusercontent.com/abikesa/creative-destruction/main/destructive.sh | bash
+curl -fsSL https://raw.githubusercontent.com/abikesa/creative-destruction/refs/heads/main/nuke.sh | bash
+
