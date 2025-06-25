ced2911ec8a8ab3a247966438e2cf63755467f7a
abikesa <144293697+abikesa@users.noreply.github.com>
2025-06-25 01:55:23 -0400

    diff uses in past

--- syntax.sh
+++ syntax.sh
@@ -1,2 +1,3 @@
+bash <(curl -s https://raw.githubusercontent.com/abikesa/directory-tree/refs/heads/main/summarize.sh) ./
 curl -fsSL https://raw.githubusercontent.com/abikesa/creative-destruction/refs/heads/main/nuke.sh | bash
 
